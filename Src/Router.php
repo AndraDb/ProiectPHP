1ba14be3b87a539441aa295da0d9ef4b09050c1c
Andra Dobritoiu <andra.dobritoiu97@gmail.com>
2019-02-02 19:22:55 +0200

    V1

---
+++ Src/Router.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Framework;
+
+class Router
+{
+
+    public function checkUrl(array $routes,string $url,string $param):void{
+
+        if(isset($routes[$url])) {
+            list($controllerObj, $action) = $this->getControllerName($routes, $url);
+            $controllerObj->{$action}();
+        }
+        else if(preg_match('/\d+/', $url, $id))
+        {
+            list($controllerObj, $action) = $this->getControllerName($routes, $url,$id[0]);
+            $controllerObj->{$action}($id[0]);
+        }
+        else{
+            echo "404 Not Found";
+        }
+    }
+
+    public function getControllerName(array $routes, string $url,int $id=null)
+    {
+        if($id){
+            $url = str_replace($id,"{id}",$url);
+        }
+        $controller = $routes[$url]['controller'];
+        $controller = "App\\Controllers\\".$controller;
+
+        $this->checkGuard($routes,$url);
+
+        $controllerObj = new $controller();
+        $action = $routes[$url]['action'];
+        return array($controllerObj, $action);
+    }
+
+    private function checkGuard(array $routes,string $route): void
+    {
+        if (isset($routes[$route]['guard'])) {
+            $guard = "\\App\\Guards\\" . $routes[$route]['guard'];
+            //instantiate and execute the handle action
+            (new $guard)->handle();
+        }
+    }
+}
